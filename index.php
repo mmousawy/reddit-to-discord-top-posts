0dd6ad67b37c2095743e1d3a2d3cbd6630b93b47
Murtada al Mousawy <murtada.al.mousawy@gmail.com>
2019-09-26 15:27:32 +0200

    feat(app): Finish script with functioning webhook posting

--- index.php
+++ index.php
@@ -6,20 +6,26 @@ class RedditExtractor
     $this->html = file_get_contents($url);
   }
 
-  function extract(): array
+  function extract(int $postAmount = 5): array
   {
-    $regex = '/class=" thing.+?(?=class=" thing|view more:)/m';
+    $postRegex = '/class=" thing.+?(?=class=" thing|view more:)/m';
 
-    preg_match_all($regex, $this->html, $matches, PREG_SET_ORDER, 0);
+    preg_match_all($postRegex, $this->html, $matches, PREG_SET_ORDER, 0);
 
     $posts = [];
 
     // Loop through the posts
     if (count($matches) > 0) {
-      foreach ($matches as $match) {
+      for ($postIndex = 0; $postIndex < $postAmount; $postIndex++) {
+        $match = $matches[$postIndex];
+
+        if (!isset($match)) {
+          break;
+        }
+
         $post = [];
 
-        $postRegex = [
+        $postValuesRegex = [
           'title' => '/data-event-action="title".+?>(.+?)</',
           'upvotes' => '/score unvoted" title="(\d+?)"/',
           'subreddit' => '/data-subreddit="(.+?)"/',
@@ -30,13 +36,17 @@ class RedditExtractor
           'date' => '/datetime="(.+?)"/'
         ];
 
-        foreach ($postRegex as $key => $regex) {
-          preg_match($regex, $match[0], $matchedValue, PREG_OFFSET_CAPTURE, 0);
+        foreach ($postValuesRegex as $key => $valueRegex) {
+          preg_match($valueRegex, $match[0], $matchedValue, PREG_OFFSET_CAPTURE, 0);
 
           $value = '';
 
           if (isset($matchedValue[1][0])) {
-            $value = $matchedValue[1][0];
+            if ($key === 'date') {
+              $value = substr($matchedValue[1][0], 0, 19);
+            } else {
+              $value = $matchedValue[1][0];
+            }
           }
 
           $post[$key] = $value;
@@ -50,13 +60,91 @@ class RedditExtractor
   }
 }
 
-// TODO: Setup extractors for each subreddit from config.json
-$extractor = new RedditExtractor('https://old.reddit.com/r/HuntShowdown/top/?sort=top&t=day');
-$extractedData = $extractor->extract();
-var_dump($extractedData);
+class RedditPostToDiscordEmbed
+{
+  function __construct(array $post)
+  {
+    $this->post = $post;
+  }
 
-$webhookUrl = '';
+  function create(): array
+  {
+    return [
+      'author' => [
+        'name' => $this->post['author'],
+        'url' => 'https://www.reddit.com/u/' . $this->post['author']
+      ],
+      'title' => $this->post['title'],
+      'description' => $this->post['upvotes']. ' upvotes - ' . $this->post['comments'] . ' comments',
+      'url' => 'https://reddit.com' . $this->post['permalink'],
+      'thumbnail' => [
+        'url' => 'https:' . $this->post['thumbnail']
+      ],
+      'timestamp' => $this->post['date']
+    ];
+  }
+}
 
-for ($postIndex = 0; $postIndex < 5; $postIndex++) {
-  // TODO: Setup and POST digestible object to Discord
+
+class DiscordWebhookPost
+{
+  function __construct(string $webhookUrl, array $data)
+  {
+    $hookObject = json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+    echo $hookObject . PHP_EOL . PHP_EOL;
+
+    $this->curl = curl_init($webhookUrl);
+
+    curl_setopt_array($this->curl, [
+      CURLOPT_URL            => $webhookUrl,
+      CURLOPT_POST           => true,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_MAXREDIRS      => 10,
+      CURLOPT_HEADER         => true,
+      CURLOPT_POSTFIELDS     => $hookObject,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_HTTPHEADER     => [
+        'Length' => strlen($hookObject),
+        'Content-Type' => 'application/json'
+      ]
+    ]);
+  }
+
+  function post(): bool
+  {
+    $response = curl_exec($this->curl);
+
+    curl_close($this->curl);
+
+    return $response;
+  }
+}
+
+$config = json_decode(file_get_contents('config.json'), true);
+
+foreach ($config as $subredditConfig) {
+  $extractor = new RedditExtractor($subredditConfig['subredditUrl']);
+  $extractedData = $extractor->extract();
+
+  $postsData = array_map(function($extractedPost) use ($config) {
+    $embed = new RedditPostToDiscordEmbed($extractedPost);
+    return $embed->create();
+  }, $extractedData);
+
+  $discordData = [
+    'username' => 'Reddit top posts to Discord',
+    'avatar_url' => 'https://mmousawy.github.io/reddit-top-posts-discord/img/avatar.png',
+    'content' => "Today's top " . $subredditConfig['postAmount'] . ' posts from https://reddit.com/r/' . $extractedData[0]['subreddit'],
+    'embeds' => $postsData
+  ];
+
+  $webhookPost = new DiscordWebhookPost($subredditConfig['webhookUrl'], $discordData);
+  $response = $webhookPost->post();
+
+  if (!$response) {
+    echo 'Error posting for: ' . $extractedData[0]['subreddit'] . PHP_EOL;
+  } else {
+    echo 'Success posting for: ' . $extractedData[0]['subreddit'] . ' -> ' . $response . PHP_EOL;
+  }
 }
